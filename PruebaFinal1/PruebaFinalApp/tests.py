565ef604447827dfd294e3d49eb7b38808f08b14
joaquin115 <joaquin.miranda115@gmail.com>
2022-07-18 01:52:48 -0300

    test

--- PruebaFinal1/PruebaFinalApp/tests.py
+++ PruebaFinal1/PruebaFinalApp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .views import *
+from .models import Publicaciones
 # Create your tests here.
+
+
+class PublicacionesTest(TestCase):
+    def setUp(self):
+        Publicaciones.objects.create(titulo="Publicacion de viaje a Brasil", pais= 2,)
+
+    def test_publicacion_nombre(self):
+        Publicacion = Publicacion.objects.get(pais = 2)
+        self.assertEqual(self.publicacion.nombre, "Publicacion de viaje a Brasil")
+
+    def test_publicacion_creada(self):
+        Publicacion = Publicacion.objects.get(pais = 2)
+        self.assertNotEquals(publicaciones, None)
